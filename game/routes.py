0a1e07ee83b18b4930de69657db1b51b2cb52471
Baranixowy <baranix96@gmail.com>
2022-07-15 10:55:14 +0200

    Add docker

--- game/routes.py
+++ game/routes.py
@@ -1,15 +1,15 @@
-from datetime import date
+from datetime import date, time
 from game import app, db
 from flask import render_template, request, session
 from random import randint
 from game.rock_paper_scissors import game_backend
 from . import User, Game
-import json
 
 
 @app.route("/", methods=["GET", "POST"])
 def mainPage():
     if request.method == "GET":
+        """Creating a new user and counting a today cumlative record and time"""
         user = User()
         db.session.add(user)
         db.session.commit()
@@ -25,6 +25,21 @@ def mainPage():
         for player in today_players:
             today_loses += player.loses
         cumulative_record = (today_wins, today_draws, today_loses)
+        cumulative_time = time(0, 0, 0)
+        for player in today_players:
+            games = Game.query.filter_by(user_id=player.id).all()
+            for game in games:
+                duration = game.gametime
+                hours = cumulative_time.hour + duration.hour
+                minutes = cumulative_time.minute + duration.minute
+                seconds = cumulative_time.second + duration.second
+                hours += minutes // 60
+                minutes %= 60
+                minutes += seconds // 60
+                seconds %= 60
+                cumulative_time = time(
+                    hour=hours, minute=minutes, second=seconds
+                )
 
         return render_template(
             "main_page.html",
@@ -32,15 +47,16 @@ def mainPage():
             today_players=today_players,
             cumulative_record=cumulative_record,
             credits=user.credits,
+            cumulative_time=cumulative_time,
         )
     if request.method == "POST":
-        divinfo = request.get_json()
-        print(divinfo)
+        """Draw an item that the computer will choose"""
+        pc_item = randint(1, 3)
         current_user_id = session["user_id"]
         user = User.query.filter_by(id=current_user_id).first()
         today_players = User.query.filter_by(date_created=date.today()).all()
-        pc_item = randint(1,3)
         user_choice = 0
+        """"Counting a today cumlative record and time when user must buy new 10 credits"""
         today_wins = 0
         today_draws = 0
         today_loses = 0
@@ -51,6 +67,22 @@ def mainPage():
         for player in today_players:
             today_loses += player.loses
         cumulative_record = (today_wins, today_draws, today_loses)
+        cumulative_time = time(0, 0, 0)
+        for player in today_players:
+            games = Game.query.filter_by(user_id=player.id).all()
+            for game in games:
+                duration = game.gametime
+                hours = cumulative_time.hour + duration.hour
+                minutes = cumulative_time.minute + duration.minute
+                seconds = cumulative_time.second + duration.second
+                hours += minutes // 60
+                minutes %= 60
+                minutes += seconds // 60
+                seconds %= 60
+                cumulative_time = time(
+                    hour=hours, minute=minutes, second=seconds
+                )
+        """Not starting a game when user buy credits"""
         if request.form["submit_button"] == "Set credits to 10":
             user.credits = 10
             db.session.commit()
@@ -65,7 +97,9 @@ def mainPage():
                 record=record,
                 today_players=today_players,
                 cumulative_record=cumulative_record,
+                cumulative_time=cumulative_time,
             )
+        """Check user choice and start a game"""
         if request.form["submit_button"] == "Rock":
             user_choice = 1
         elif request.form["submit_button"] == "Paper":
@@ -73,6 +107,7 @@ def mainPage():
         elif request.form["submit_button"] == "Scissors":
             user_choice = 3
         result = game_backend(user_choice, pc_item)
+        """Change amount of credits along with player and cumulative record"""
         if user.credits > 3:
             user.credits -= 3
         else:
@@ -102,9 +137,33 @@ def mainPage():
         for player in today_players:
             today_loses += player.loses
         cumulative_record = (today_wins, today_draws, today_loses)
-        game = Game(user_id=current_user_id, credits=user.credits, result = game_result)
+        """Checking a game time and add a game statistic to database"""
+        inputminutes = int(request.form["inputminutes"])
+        inputseconds = int(request.form["inputseconds"])
+        game_duration = time(0, inputminutes, inputseconds)
+        game = Game(
+            user_id=current_user_id,
+            credits=user.credits,
+            result=game_result,
+            gametime=game_duration,
+        )
         db.session.add(game)
         db.session.commit()
+        cumulative_time = time(0, 0, 0)
+        for player in today_players:
+            games = Game.query.filter_by(user_id=player.id).all()
+            for game in games:
+                duration = game.gametime
+                hours = cumulative_time.hour + duration.hour
+                minutes = cumulative_time.minute + duration.minute
+                seconds = cumulative_time.second + duration.second
+                hours += minutes // 60
+                minutes %= 60
+                minutes += seconds // 60
+                seconds %= 60
+                cumulative_time = time(
+                    hour=hours, minute=minutes, second=seconds
+                )
         return render_template(
             "main_page.html",
             result=result,
@@ -114,4 +173,5 @@ def mainPage():
             record=record,
             today_players=today_players,
             cumulative_record=cumulative_record,
+            cumulative_time=cumulative_time,
         )
